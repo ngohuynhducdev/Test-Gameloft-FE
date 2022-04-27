0206ebe657666753d2257991277b245c761be33c
ngohuynhducdev <ngohuynhducdev@gmail.com>
2022-04-27 10:28:18 +0700

    update

--- src/features/HomePage/HomePage.js
+++ src/features/HomePage/HomePage.js
@@ -1,18 +1,21 @@
-import React from "react";
-
-import StickyMenu from "./components/StickyMenu/StickyMenu.js";
+import React, { useEffect, useState } from "react";
 import ContactForm from "./components/ContactForm/ContactForm.js";
-import HotspotBanner from "./components/HotspotBanner/HotspotBanner.js";
-import Promotion from "./components/Promotion/Promotion.js";
 import ExclusiveContent from "./components/ExclusiveContent/ExclusiveContent.js";
 import Footer from "./components/Footer/Footer.js";
+import HotspotBanner from "./components/HotspotBanner/HotspotBanner.js";
+import Promotion from "./components/Promotion/Promotion.js";
+import StickyMenu from "./components/StickyMenu/StickyMenu.js";
 
 import { FullPage, Slide } from "react-full-page";
-
 export default function HomePage() {
+  const { width } = useWindowDimensions();
   return (
     <>
-      <FullPage controls="true" duration={100}>
+      <FullPage
+        controls="true"
+        duration={100}
+        scrollMode={width >= 1920 ? "full-page" : "normal"}
+      >
         <StickyMenu />
         <Slide>
           <HotspotBanner />
@@ -32,4 +35,29 @@ export default function HomePage() {
       </FullPage>
     </>
   );
+}
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
 }
